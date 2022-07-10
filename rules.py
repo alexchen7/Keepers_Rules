fe12f286468c121413d76c24e6831ba5a846e0cc
Aちゃん <alex.chen7@163.com>
2022-07-11 00:27:45 +0800

    Non_Official_Group_Update
    Main update for unofficial group rules.py and non-official keepers

--- rules.py
+++ rules.py
@@ -18,23 +18,23 @@ max_seeders_for_salary = 0, min_size_first_adoption_ratio = 0.5 ,comment = ''):
 	""" Takes uid and optional assessment parameters. Save the
 	customized assessment scheme to database."""
 	uid = input("请输入uid:")
-	
+
 	# input bundle
 	bundle = input('请输入数字1至5选择套餐：1.酱油 2.实习 3.正式 4.资深 5.特招, \
-	其余输入视为默认输入,enter进入下一步')
-	
+	6.非官方10TB 其余输入视为默认输入,enter进入下一步')
+
 	# prevent null input
 	if bundle:
 		if bundle == '1':
 			min_size_adopted = 2048
 			salary_ratio = 0.4
-			comment = '酱油保种员'	
+			comment = '酱油保种员'
 			print ('您选择了酱油保种员')
 		elif bundle == '2':
 			min_size_adopted = 4096
 			salary_ratio = 0.6
-			comment = '实习保种员'	
-			print ('您选择了实习保种员')			
+			comment = '实习保种员'
+			print ('您选择了实习保种员')
 		elif bundle == '3':
 			min_size_adopted = 6144
 			salary_ratio = 0.8
@@ -49,10 +49,18 @@ max_seeders_for_salary = 0, min_size_first_adoption_ratio = 0.5 ,comment = ''):
 			salary_ratio = 0.8
 			comment = '特招保种员'
 			print ('您选择了特招保种员')
-	
+		elif bundle == '6':
+			min_size_adopted = 10240
+			salary_ratio = 0.33
+			comment = '非官方10TB'
+			code_official_group = '33 30 27 26 25 24'
+			code_invalid_category = '0000'
+
+			print ('您选择了非官方保种员')
+
 	# create a set of exitsted uid
 	existed_uid = set()
-	
+
 	# initialize min_size_first_adoption
     # confirm the parameters
 	print ('请再次检查以下参数')
@@ -76,19 +84,19 @@ max_seeders_for_salary = 0, min_size_first_adoption_ratio = 0.5 ,comment = ''):
 	'即第一认领最少体积为：',\
 	min_size_first_adoption_ratio * min_size_adopted, 'GB', '\n', \
 	'备注：', comment)
-	
+
 	confirm = str(input("Enter 'yes' if to proceed, otherwise exit"))
-	
 
 
-	
+
+
     # exit if not confirmed
 	if confirm != 'yes':
 		print ('Aborted!')
 		return
-	
+
 	filename = 'rules'
-	
+
 	with open(filename, 'a', newline = '', encoding = 'utf-8') as f:
 		fieldnames = ['用户id', '最小单种体积', '最少总认领体积', \
 		'最少总认领数', '最少做种时间', \
@@ -97,23 +105,23 @@ max_seeders_for_salary = 0, min_size_first_adoption_ratio = 0.5 ,comment = ''):
 		'工资比例', '工资体积系数',	'工资种子寿命系数', \
 		'工资做种时间系数', '工资做种人数系数', '最多允许同伴数', \
 		'最少第一认领占体积比', '备注']
-	
+
 		thewriter = csv.DictWriter(f, fieldnames = fieldnames)
-    
+
         # decide to write or not to write the title
 		if not (f.tell()):
 			thewriter.writeheader()
-	
+
 		# get all the existed uid in a set to aovid dup rules
 		rf = csv.DictReader(open(filename, encoding = 'utf-8'))
 		for i in rf:
 			existed_uid.add(i['用户id'])
-		
+
 		# in case input is a list
 		if isinstance(uid, list):
-			
+
 			for this_uid in uid:
-				
+
 				if this_uid not in existed_uid:
 					thewriter.writerow({'用户id':this_uid, \
 					'最小单种体积': min_size_torrent, \
@@ -134,10 +142,10 @@ max_seeders_for_salary = 0, min_size_first_adoption_ratio = 0.5 ,comment = ''):
 					'最少第一认领占体积比': min_size_first_adoption_ratio, \
 					'备注': comment})
 					print ('成功设置{}的规则'.format(uid))
-					
+
 				else:
 					print ('错误！该id已设置规则!')
-					
+
 		# in case it's not a list
 		else:
 			if uid not in existed_uid:
@@ -158,10 +166,10 @@ max_seeders_for_salary = 0, min_size_first_adoption_ratio = 0.5 ,comment = ''):
 				'工资做种人数系数': seeders_for_salary_ratio, \
 				'最多允许同伴数': max_seeders_for_salary, \
 				'最少第一认领占体积比': min_size_first_adoption_ratio, \
-				'备注': comment})	
-				print ('成功设置{}的规则'.format(uid))	
+				'备注': comment})
+				print ('成功设置{}的规则'.format(uid))
 			else:
 				print ('错误！',uid,'该id已设置规则!')
-                
+
 if __name__ == "__main__":
     write_rule()
